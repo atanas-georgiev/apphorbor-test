c680e7ebbd9dff0b44af8adba654cf3e9b43764c
atanas-georgiev <atanasgeorgiev83@gmail.com>
2015-11-16 12:33:36 +0200

    WCF

--- Server/MediaSystem.Api/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MediaSystem.Api.WebApiApplication" Language="C#" %>
--- WcfServiceChat/WcfServiceChat/Properties/AssemblyInfo.cs
+++ WcfServiceChat/WcfServiceChat/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MediaSystem.Data")]
+[assembly: AssemblyTitle("WcfServiceChat")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MediaSystem.Data")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Johnson Controls")]
+[assembly: AssemblyProduct("WcfServiceChat")]
+[assembly: AssemblyCopyright("Copyright © Johnson Controls 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f8e9237c-c66f-4eaa-93f0-26d7661cfcdf")]
+[assembly: Guid("304efddd-5dda-41ee-b0c3-3b694db341b3")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
